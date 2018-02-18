53d2eaadd0307606df09de4739f353439a1b0e1e
jgbkid <mvenice66@gmail.com>
2018-02-17 16:02:58 -0800

    Text color change
    Changed to white

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let textColor = 'lime';
+let textColor = 'white';
 let defaultStyle = {
   color: textColor
 };
